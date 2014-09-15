c8a70c14d8bc55bf91af589bdc61594490e1023a
MarconiVini <cardosounicamp@gmail.com>
2014-09-15 20:50:11 -0300

    Ajusting routes to singular

--- spec/requests/v1/v1_streams_spec.rb
+++ spec/requests/v1/v1_streams_spec.rb
@@ -23,36 +23,36 @@ RSpec.describe "V1::Streams", :type => :request do
 
 		
     it "expect route to define user token" do
-      skip
+      #skip
       #status 401 means unauthorized / should match "render status: :unauthorized" in controller
-      get "v1/devices/#{@device.id}/streams"
+      get "v1/device/#{@device.id}/streams"
       expect(response.status).to be 401
-      get "v1/devices/#{@device.id}/streams/last"
+      get "v1/device/#{@device.id}/streams/last"
       expect(response.status).to be 401
-      get "v1/devices/#{@device.id}/streams/#{@time_stamp.strftime('%d-%m-%Y')}"
+      get "v1/device/#{@device.id}/streams/#{@time_stamp.strftime('%d-%m-%Y')}"
       expect(response.status).to be 401
-      get "v1/devices/#{@device.id}/streams/#{@time_stamp.strftime('%d-%m-%Y')}/#{@time_stamp2.strftime('%d-%m-%Y')}"
+      get "v1/device/#{@device.id}/streams/#{@time_stamp.strftime('%d-%m-%Y')}/#{@time_stamp2.strftime('%d-%m-%Y')}"
       expect(response.status).to be 401
     end
 
     it "expect route to return correct json length" do
-      get "v1/devices/#{@device.id}/streams?token=#{@user.token}"
+      get "v1/device/#{@device.id}/streams?token=#{@user.token}"
       expect(JSON.parse(response.body).length).to be(3)
     end
 
     it "expect route to return correct json length" do
-      get "v1/devices/#{@device.id}/streams/#{@time_stamp.strftime('%d-%m-%Y')}?token=#{@user.token}"
+      get "v1/device/#{@device.id}/streams/#{@time_stamp.strftime('%d-%m-%Y')}?token=#{@user.token}"
       expect(JSON.parse(response.body).length).to be(1)
     end
 
     it "expect route to return correct json length" do
-      get "v1/devices/#{@device.id}/streams/#{@time_stamp.strftime('%d-%m-%Y')}/#{@time_stamp2.strftime('%d-%m-%Y')}?token=#{@user.token}"
+      get "v1/device/#{@device.id}/streams/#{@time_stamp.strftime('%d-%m-%Y')}/#{@time_stamp2.strftime('%d-%m-%Y')}?token=#{@user.token}"
       expect(JSON.parse(response.body).length).to be(3)
     end
 
     it "should return only the last json" do
       skip
-      get "v1/devices/#{@device.id}/streams/last?token=#{@user.token}"
+      get "v1/device/#{@device.id}/streams/last?token=#{@user.token}"
       json = JSON.parse(response.body)
       expect(json.length).to be(1)
       expect(json["body"]).to eql({"test" => "test3"})
