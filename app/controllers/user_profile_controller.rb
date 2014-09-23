9c9ce156c69b7042deb24b3b7e7836adfa518289
Carlos Eduardo <kadu@kadu.com.br>
2014-09-23 06:07:55 -0300

    Squashed commit of the following:
    commit cd94924ea702f40cba9f3811479a304ef9b72cef
    Author: Carlos Eduardo <kadu@kadu.com.br>
    Date:   Tue Sep 23 06:05:07 2014 -0300
    
        User Profile
    
    commit d1043fd3275dbf50262bb5030c0b39e1f0c7e765
    Author: Carlos Eduardo <kadu@kadu.com.br>
    Date:   Tue Sep 23 06:04:48 2014 -0300
    
        User Profile

---
+++ app/controllers/user_profile_controller.rb
@@ -0,0 +1,5 @@
+class UserProfileController < ApplicationController
+  def index
+    @user = current_user
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@
 
 Rails.application.routes.draw do
 
+  get 'user_profile/index'
+
   root 'home#index'
 
   devise_for :users
