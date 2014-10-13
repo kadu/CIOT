821ccd764e36bd63a239608cc51f41f10554f5a9
Carlos Eduardo <kadu@kadu.com.br>
2014-10-13 15:18:50 -0300

    Spaces

--- app/mailers/trigger_mail.rb
+++ app/mailers/trigger_mail.rb
@@ -1,7 +1,7 @@
 class TriggerMail < ActionMailer::Base
   default from: 'from@example.com'
 
-  def welcome_email(mailer)
-    mail(to: mailer, subject: 'Testing email')
-  end
+  def welcome_email(mailer)
+    mail(to: mailer, subject: 'Testing email')
+  end
 end
